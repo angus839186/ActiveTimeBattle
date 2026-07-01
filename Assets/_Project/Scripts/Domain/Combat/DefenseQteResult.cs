389efbb4660ae9e47a8c0a585ab3a6925faab2dd
徐英森 <96182289+angus839186@users.noreply.github.com>
2026-07-01 22:38:14 +0800

    清除codex相關

--- Assets/_Project/Scripts/Domain/Combat/DefenseQteResult.cs
+++
@@ -1,9 +0,0 @@
-namespace ActiveTimeBattle.Domain.Combat
-{
-    public enum DefenseQteResult
-    {
-        Broken,
-        Normal,
-        Perfect
-    }
-}
--- Assets/_Project/Scripts/Domain/Exploration/CampChoice.cs
+++
@@ -1,9 +0,0 @@
-namespace ActiveTimeBattle.Domain.Exploration
-{
-    public enum CampChoice
-    {
-        Heal,
-        Money,
-        Leave
-    }
-}
